5996208a02fb0c35a4e22674f67999bde4f062bd
bayu <loshe37@gmail.com>
2023-11-21 09:46:09 +0700

    update electron patch 1

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-const {app,BrowserWindow} = require('electron')
+const {app,BrowserWindow,ipcMain} = require('electron')
 const path = require('path')
 const {autoUpdater} = require('electron-updater')
 const log = require('electron-log');
-log.transports.file.resolvePath = () => path.join('C:/xampp8.1.10/htdocs/newcoba?electron/auto-update-electron-master/logs', '/logs/main.log');
+log.transports.file.resolvePath = () => path.join('C:/xampp8.1.10/htdocs/newcoba/electron/auto-update-electron-master/', '/logs/main.log');
 log.info('Hello, log');
 log.warn('Some problem appears');
+const appVersion = app.getVersion();
 let win;
+const dispatch = (data) => {
+    win.webContents.send('message', data)
+}
 function createWindow(){
-win = new BrowserWindow({width:300,height:400})
+win = new BrowserWindow({width:300,height:400,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
+})
+// const feedURL = 'file:///path/to/your/local/repo';
+
+autoUpdater.updateConfigPath = path.join(__dirname, 'dev-app-update.yml');
+console.log(path.join(__dirname, 'dev-app-update.yml'))
 
 win.loadFile(path.join(__dirname,'index.html'))
 }
@@ -16,19 +29,45 @@ app.on('ready',()=>{
     createWindow()
     autoUpdater.checkForUpdatesAndNotify()
 })
+ipcMain.on('first-notif', (event, data) => {
+    if (!win.isDestroyed()) {
+        win.webContents.send('versi-aplikasi', 'v'+ appVersion);
+        win.webContents.send('version', 'v'+ appVersion);
+    }
+});
 
-
-autoUpdater.on("update-available",()=>{
-   log.info("update-available")
-})
-
-autoUpdater.on("checking-for-update",()=>{
+autoUpdater.on('checking-for-update', () => {
+    dispatch('Checking for update...')
     log.info("checking-for-update")
-})
-
-autoUpdater.on("download-progress",()=>{
+  })
+  
+  autoUpdater.on('update-available', (info) => {
+    dispatch('Update available.')
+    log.info("update-available")
+    })
+  
+  autoUpdater.on('update-not-available', (info) => {
+    dispatch('Update not available.')
+  })
+  
+  autoUpdater.on('error', (err) => {
+    dispatch('Error in auto-updater. ' + err)
+  })
+  
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+    dispatch(log_message)
     log.info("download-progress")
-})
-autoUpdater.on("update-downloaded",()=>{
+  
+    win.webContents.send('size', log_message)
+    win.webContents.send('download-progress', progressObj.percent)
+  
+  })
+  
+  autoUpdater.on('update-downloaded', (info) => {
+    dispatch('Update downloaded')
     log.info("update-downloaded")
-})
+  })
+  
---
+++ renderer.js
@@ -0,0 +1,27 @@
+const { ipcRenderer } = require('electron');
+// renderer.js
+// import { ipcRenderer } from `${__dirname}/electron`;
+
+const select = selector => document.querySelector(selector)
+
+let container = select('#messages')
+let progressBar = select('#progressBar')
+let version = select('#version')
+ipcRenderer.send('first-notif', 'Data yang dikirim dari Vue Index');
+
+ipcRenderer.on('message', (event, text) => {
+
+  let message = document.createElement('div')
+  message.innerHTML = text
+  container.appendChild(message)
+
+})
+
+ipcRenderer.on('version', (event, text) => {
+  console.log(text);
+  version.innerText = text
+})
+
+ipcRenderer.on('download-progress', (event, text) => {
+  progressBar.style.width = `${text}%`
+})
